db85b3e4deb466899cbacf122d83c063cb67f602
Louis Bettens <louis@bettens.info>
2020-06-14 23:51:00 +0200

    Fix tests

--- src/main/scala/utry/package.scala
+++ src/main/scala/utry/package.scala
@@ -32,7 +32,7 @@ object UTry:
     def toOption = self.fold(_ => None)(Some(_))
     def foreach(f: A => Unit): Unit = self.fold(_ => ())(a => f(a))
     def filter(f: A => Boolean): UTry[A] = self.fold(e => e)(a => if f(a) then self else NoSuchElementException("filtered"))
-    def failed: UTry[Throwable] = self.fold(e => e.wrap)(_ => NoSuchElementException("Success"))
+    def failed: UTry[Throwable] = self.fold(e => e.wrap)(_ => UnsupportedOperationException("Success"))
 
     // FIXME: should be lazy
     def withFilter(f: A => Boolean) = self.filter(f)
@@ -41,13 +41,13 @@ object UTry:
     def exception = self.fold(e => e)(_ => throw NoSuchElementException("Success"))
 
   extension UTryMapOps on [A, B](self: UTry[A]):
-    def map(f: A => B): UTry[B] = self.fold(e => e)(a => f(a).wrap)
-    def flatMap(f: A => UTry[B]): UTry[B] = self.fold(e => e)(a => f(a))
+    def map(f: A => B): UTry[B] = self.fold(e => e)(a => UTry(f(a)))
+    def flatMap(f: A => UTry[B]): UTry[B] = self.fold(e => e)(a => try f(a) catch case util.control.NonFatal(e) => e)
     def transform(s: A => UTry[B], f: Throwable => UTry[B]): UTry[B] =
       self.fold(f)(s)
 
   extension UTryFoldOps on [A, B](self: UTry[A]):
-    def fold(fe: (Throwable) => B, fa: (A) => B): B = impl.fold(self)(fe)(fa)
+    def fold(fe: (Throwable) => B, fa: (A) => B): B = try impl.fold(self)(fe)(fa) catch case util.control.NonFatal(e) => fe(e)
 
   extension UTryFlattenOps on [A <: UTry[B], B](self: UTry[A]):
     def flatten: UTry[B] = self.fold(e => e)(a => a)
