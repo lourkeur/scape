ae62dedb98aa7aaab8d7856b7d41a600f391e487
Louis Bettens <louis@bettens.info>
2020-06-17 12:27:57 +0200

    Refactor

--- src/main/scala/utry/package.scala
+++ src/main/scala/utry/package.scala
@@ -16,106 +16,11 @@ object USuccess:
   def unapply[A](ta: UTry[A]): Option[A] =
     impl.fold(ta)(_ => None)(Some[A])
 
-object UTry extends ext:
+object UTry:
   def apply[A](a: => A): UTry[A] =
     try USuccess[A](a)
     catch
       case NonFatal(e) => UFailure(e)
 
-trait ext:
-  extension UTryOps on [A](self: UTry[A]):
-    def isSuccess = self match
-      case USuccess(_) => true
-      case _ => false
-
-    def isFailure = self match
-      case UFailure(_) => true
-      case _ => false
-
-    def get: A = self match
-      case UFailure(e) => throw e
-      case USuccess(a) => a
-
-    def toEither = self match
-      case UFailure(e) => Left(e)
-      case USuccess(a) => Right(a)
-
-    def toOption = self match
-      case UFailure(_) => None
-      case USuccess(a) => Some(a)
-
-    def foreach(f: A => Unit): Unit = self match
-      case USuccess(a) => f(a)
-      case _ =>
-
-    def filter(pred: A => Boolean): UTry[A] = self match
-      case USuccess(a) if pred(a) => self
-      case UFailure(_) => self
-      case _ => UFailure(NoSuchElementException("filtered"))
-
-    def failed: UTry[Throwable] = self match
-      case UFailure(e) => USuccess(e)
-      case _ => UFailure(UnsupportedOperationException("Success"))
-
-    def exception: Throwable = self match
-      case UFailure(e) => e
-      case _ => UnsupportedOperationException("Success")
-
-    // FIXME: should be lazy
-    def withFilter(f: A => Boolean) = self.filter(f)
-
-    def fold = ext.FoldPartiallyApplied(self)
-    def transform = ext.TransformPartiallyApplied(self)
-    def getOrElse = ext.GetOrElsePartiallyApplied(self)
-    def orElse = ext.OrElsePartiallyApplied(self)
-    def map = ext.MapPartiallyApplied(self)
-    def flatMap = ext.FlatMapPartiallyApplied(self)
-
-    def recover = ext.RecoverPartiallyApplied(self)
-    def recoverWith = ext.RecoverWithPartiallyApplied(self)
-
-  extension UTryFlatten on [A](self: UTry[UTry[A]]):
-    def flatten: UTry[A] = self match
-      case UFailure(e) => UFailure(e)
-      case USuccess(a) => a
-
-object ext:
-  class FoldPartiallyApplied[A](self: UTry[A]) extends AnyVal:
-    def apply[B](fe: Throwable => B, fa: A => B) = self match
-      case UFailure(e) => fe(e)
-      case USuccess(a) => try fa(a) catch case NonFatal(e) => fe(e)
-
-  class TransformPartiallyApplied[A](self: UTry[A]) extends AnyVal:
-    def apply[B](fa: A => UTry[B], fe: Throwable => UTry[B]): UTry[B] = self match
-      case UFailure(e) => fe(e)
-      case USuccess(a) => fa(a)
-
-  class GetOrElsePartiallyApplied[A](self: UTry[A]) extends AnyVal:
-    def apply[B >: A](default: => B): B = self match
-      case UFailure(e) => default
-      case USuccess(a) => a
-
-  class OrElsePartiallyApplied[A](self: UTry[A]) extends AnyVal:
-    def apply[B >: A](default: => UTry[B]): UTry[B] = self match
-      case UFailure(e) => default
-      case USuccess(a) => USuccess(a)
-
-  class MapPartiallyApplied[A](self: UTry[A]) extends AnyVal:
-    def apply[B](f: A => B): UTry[B] = self match
-      case UFailure(e) => UFailure(e)
-      case USuccess(a) => UTry(f(a))
-
-  class FlatMapPartiallyApplied[A](self: UTry[A]) extends AnyVal:
-    def apply[B](f: A => UTry[B]): UTry[B] = self match
-      case UFailure(e) => UFailure(e)
-      case USuccess(a) => try f(a) catch case NonFatal(e) => UFailure(e)
-
-  class RecoverPartiallyApplied[A](self: UTry[A]) extends AnyVal:
-    def apply[B >: A](pf: PartialFunction[Throwable, B]): UTry[B] = self match
-      case UFailure(pf(b)) => USuccess(b)
-      case _ => self
-
-  class RecoverWithPartiallyApplied[A](self: UTry[A]) extends AnyVal:
-    def apply[B >: A](pf: PartialFunction[Throwable, UTry[B]]): UTry[B] = self match
-      case UFailure(pf(tb)) => tb
-      case _ => self
+  export ext.{given _}
+end UTry
