fdc3a7fcc7be4afb6773bfc9a3331f7594438bc6
Louis Bettens <louis@bettens.info>
2020-06-17 13:57:10 +0200

    Rename

--- src/main/scala/utry/impl.scala
+++ src/main/scala/utry/impl.scala
@@ -1,17 +1,17 @@
 package utry.impl
 
-case class WrappedThrowable[+A](val unwrap: A)
+case class Escape[+A](unescape: A)
 
 type UFailure[+A] = Throwable
-type USuccess[+A] = WrappedThrowable[A] | A
+type USuccess[+A] = Escape[A] | A
 type UTry[+A] = UFailure[A] | USuccess[A]
 
 inline def escape[A](a: A): USuccess[A] = a match
-  case _: WrappedThrowable[_] | _: Throwable => WrappedThrowable(a)
+  case _: Escape[_] | _: Throwable => Escape(a)
   case _ => a
 
 // FIXME: inline goes boom
 def fold[A, B](ta: UTry[A])(fe: Throwable => B)(fa: A => B) = ta match
   case e: Throwable => fe(e)
-  case WrappedThrowable(a) => fa(a.asInstanceOf[A])
+  case Escape(a) => fa(a.asInstanceOf[A])
   case a => fa(a.asInstanceOf[A])
