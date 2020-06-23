d81cc595c4b3f51c55f436667e5f068b229c224b
Louis Bettens <louis@bettens.info>
2020-06-23 10:41:46 +0200

    Fix naming

--- src/test/scala/scape/UTrySuite.scala
+++ src/test/scala/scape/UTrySuite.scala
@@ -3,9 +3,9 @@ package scape
 import org.scalacheck._
 import cats.instances.all.{given cats.Eq[?]}
 
-object UOptionSuite extends Properties("scape"):
+object UTrySuite extends Properties("UTry"):
   include(impl.ImplTest[Int, Int].all)
 
   property("regression test for #2") =
     USuccess(null).isSuccess
-end UOptionSuite
+end UTrySuite
