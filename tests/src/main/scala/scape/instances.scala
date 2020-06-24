1a342b00459fc032e8d9743d5e8dd5b41cdfe332
Louis Bettens <louis@bettens.info>
2020-06-24 19:48:24 +0200

    Add tests.Diverse

--- tests/src/main/scala/scape/instances.scala
+++ tests/src/main/scala/scape/instances.scala
@@ -15,3 +15,29 @@ given [A: Cogen] as Cogen[UTry[A]] =
     case UFailure(e) => Failure(e)
     case USuccess(a) => Success(a)
   }
+
+
+/** A varied selection of values for good measure law testing */
+opaque type Diverse = Int | Boolean | Null | String | UTry[Any/*Diverse*/]
+
+object Diverse:
+  given cats.Eq[Diverse] = cats.Eq.fromUniversalEquals
+
+  given Arbitrary[Diverse] = Arbitrary {
+    Gen.frequency(
+      30 -> Arbitrary.arbitrary[Int],
+      5 -> Arbitrary.arbitrary[Boolean],
+      5 -> Gen.const(null),
+      40 -> Arbitrary.arbitrary[String],
+      10 -> Gen.lzy(Arbitrary.arbitrary[UTry[Diverse]]),
+    )
+  }
+
+  given Cogen[Diverse] = Cogen { (seed, x) =>
+    x.match
+      case x: Int => Cogen.perturb(seed, x)
+      case x: Boolean => Cogen.perturb(seed, x)
+      case null => seed
+      case x: String => Cogen.perturb(seed, x)
+      case x: UTry[Diverse] => Cogen.perturb(seed, x)
+  }
--- tests/src/test/scala/scape/UTrySuite.scala
+++ tests/src/test/scala/scape/UTrySuite.scala
@@ -10,11 +10,11 @@ import cats._
 object UTrySuite extends Properties("UTry"):
   given Eq[Throwable] = Eq.fromUniversalEquals
 
-  include(EqTests[UTry[Int]].eqv.all)
-  include(MonadErrorTests[UTry, Throwable].monadError[Int, Int, Int].all)
-  include(CoflatMapTests[UTry].coflatMap[Int, Int, Int].all)
-  include(TraverseTests[UTry].traverse[Int, Int, Int, Set[Int], Option, Option].all)
-  include(MonoidTests[UTry[Int]].monoid.all)
+  include(EqTests[UTry[Diverse]].eqv.all)
+  include(MonadErrorTests[UTry, Throwable].monadError[Diverse, Diverse, Diverse].all)
+  include(CoflatMapTests[UTry].coflatMap[Diverse, Diverse, Diverse].all)
+  include(TraverseTests[UTry].traverse[Int, Diverse, Diverse, Set[Int], Option, Option].all)
+  include(MonoidTests[UTry[String]].monoid.all)
 
   property("regression test for #2") =
     USuccess(null).isSuccess
